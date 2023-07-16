2d1489dacc7194739a02c8d2c2c4ddcee1fa0d2b
muhammadAzeem <azeemmuhammad0150@gmail.com>
2023-07-16 07:37:47 +0500

    first commit

---
+++ Chapter1_Exercise.py
@@ -0,0 +1,20 @@
+import openai
+
+openai.api_key="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"
+
+# Requirements
+# Write a code so that we have the most random possible answer. Meaning setting the value that determines randomness to max
+# Generate 6 responses and pick the best among them
+# The completion token limit should be set 25
+# Do not include any argument that are not necessary to the requirements
+
+prompts ="Write a tagline for an ice cream shop"
+
+response = openai.Completion.create(model="text-davinci-002", 
+                                    prompt=prompts,
+                                    n=6,
+                                    temperature=1,
+                                    max_tokens=25,
+                                    best_of=6
+                                    )
+print(response)
