80df29ccd2e8dd3bb4eec8a7538de488fb019d90
muhammadAzeem <92116797+muhammadAzeem0x000@users.noreply.github.com>
2023-07-16 16:03:37 +0500

    Updated the API key

--- 2. With_n_parameter.py
+++ 2. With_n_parameter.py
@@ -1,7 +1,7 @@
 import os
 import openai
 import secret
-openai.api_key="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"
+openai.api_key="OPENAI_API_KEY"
 
 # Codio solution begins
 prompts ="Write a tagline for an ice cream shop"
